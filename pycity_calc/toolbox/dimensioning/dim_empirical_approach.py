e69ddaee81719806b77fbb7a8ca6eb1ecdf849d0
Julius Unrath <julius.unrath@posteo.de>
2017-08-02 11:24:29 +0200

    MT Heatpump (Air/Water) with real specs integrated Showing diagram of heatpump

--- pycity_calc/toolbox/dimensioning/dim_empirical_approach.py
+++ pycity_calc/toolbox/dimensioning/dim_empirical_approach.py
@@ -17,6 +17,7 @@ Passende Szenarien anhand der Randbedingungen auswählen und in qual_scenarios s
 
 import os
 import pickle
+import matplotlib.pyplot as plt
 import pycity_calc.cities.city as City
 from prettytable import PrettyTable
 
@@ -36,13 +37,11 @@ def run_approach(city):
 
     solutions = [] #Liste aller möglichen dimensionierten Szenarien (inklusive Kosten und Emissionen)
 
-    building_age = get_building_age(city)
     building_con = True
     heating_net = True
     geothermal = True
+
     # building_con, heating_net, geothermal = additional_information()
-
-
 
 #----------------------- Abschätzung ob zentral/dezentral versorgt werden sollte ----------------------------------
 
@@ -70,10 +69,12 @@ def run_approach(city):
             if 'centralized' in scenario['type']:
                 if approve_scenario(city, scenario, geothermal):
                     solutions.append(dim_centralized(city,scenario))
+
+
         print('\n##### Centralized #####')
-        t = PrettyTable(['Scenario', 'base', 'peak'])
+        t = PrettyTable(['Scenario', 'devices'])
         for idx, val in enumerate(solutions):
-            t.add_row(['Scenario ' + str(idx), val[0], val[1]])
+            t.add_row(['Scenario ' + str(idx), val])
         print(t)
 
     elif dhn_elig < 3:
@@ -94,12 +95,14 @@ def run_approach(city):
         #             print('dim_decentralized mit', scenario)
         #             solutions.append(dim_decentralized(city,scenario))
 
-def get_building_age(city):
+
+
+
+
+def get_building_age(city): #alternativ immer exaktes Alter abfragen. Wie sähe ein Neubau aus? build_year = 2017?
     for house in city.node.values():
         if house['entity'].build_year >= 2009: # abhängig von Inkrafttreten des EEWärmeG (01.01.2009)
-            print('New buildings')
             return 'new'
-    print('No new buildings')
     return 'old'
 
 
@@ -186,7 +189,7 @@ def approve_scenario(city, scenario, geothermal):
             return False
 
     if 'hp_geo' in scenario['base'] and not geothermal:
-            return False
+        return False
 
     return True
 
@@ -280,10 +283,16 @@ def dim_centralized(city, scenario):
     :return: scenario with sizes of devices - in welcher form? city_object
     '''
 
-    base = []
-    peak = []
+    building_age = get_building_age(city)
+    if building_age == 'new':
+        print('Check if requirements of EEWärmeG are fulfilled!')
+
+    result = {}
+
     # get thermal and electrical demand curves (only available for sanitation)
     [el_curve, th_curve] = city.get_power_curves(current_values=False)
+
+    # Gleichzeitigkeitsfaktor nicht benötigt, da konkrete Werte zur Dimensionierung benutzt werden
 
     # Load duration curve (Jahresdauerlinie)
     th_LDC = get_LDC(th_curve)
@@ -297,8 +306,9 @@ def dim_centralized(city, scenario):
             # BHKW immer mit Speicher
 
             # Dimensionierungsstrategie: 1.Anlage bei 5500h/a mit 1/2 mehr installierter Leistung als nötig für Speicher
-            q_base_max.append(3/2*th_LDC[5500])
-            base.append('CHP: ' + str(round(q_base_max[-1]/1000)) +'kW (' + str(round(q_base_max[-1]/max(th_curve)*100)) + '%)')
+            chp_inst = 3/2*th_LDC[5500]
+
+            result['chp'] = chp_inst # in W
 
             # Dimensionierung nach Krimmling "Energieeffiziente Nahwärmesysteme", S.127 - Faustformel: BHKW-Leistung zwischen 10% und 20% der Maximallast
             # q_borders = [min(th_LDC, key=lambda x:abs(x-0.1*max(th_LDC))), min(th_LDC, key=lambda x:abs(x-0.2*max(th_LDC)))] #gibt die Leistungsgrenzen nach Faustformel aus
@@ -306,26 +316,87 @@ def dim_centralized(city, scenario):
             # print('BHKW mit Leistung zwischen ', q_borders)
             # print('Betriebszeiten zwischen ', t_borders)
 
+            if 'boiler' in scenario['peak']:
+                boiler_inst = max(th_LDC) - chp_inst
+                result['boiler'] = boiler_inst
+
 
 
         elif device == 'solar': # Regeln für Dimensionierung einführen!
             pv_area = 0
             for building in city.node.values():
                 pv_area += building['entity'].roof_usabl_pv_area
-            base.append('Solarth.: ' + str(round(pv_area)) + 'm²')
+            result['solar'] = pv_area # in m2
 
         elif device == 'hp_geo':
-            base.append('HP_geo: ***')
+            result['hp_geo'] = 1 # dummy
+
+
 
         elif device == 'hp_air':
-            base.append('HP air: ***')
 
-    q_peak_max = []
-    for device in scenario['peak']:
-        if device == 'boiler':
-            q_peak_max.append(max(th_LDC) - sum(q_base_max))
-            peak.append('Boiler: ' + str(round(q_peak_max[-1]/1000)) + 'kW (' + str(round(q_peak_max[-1]/max(th_curve)*100)) + '%)')
-    return [base, peak]
+            # Vorlauftemperaturen für Heizkörper? Fußbodenheizung?
+            # check if tFlow = 60°C.
+            tmax_dhw = 0
+            for building in city.node.values():
+                for apartment in building['entity'].apartments:
+                    dhw_t = apartment.demandDomesticHotWater.tFlow
+                    if dhw_t > tmax_dhw:
+                        tmax_dhw = dhw_t
+                        print('tMax for dhw set to ', dhw_t)
+
+            t_min_env = min(city.environment.weather.tAmbient)
+            q_max_distr = max(city.get_aggr_space_h_power_curve(current_values=False)) # W
+
+            if tmax_dhw <= 55:
+                print('LT HeatPump')
+
+            elif tmax_dhw > 55 and dhw_t <= 65:
+
+                # Details HeatPump
+                t1_hp = -15 #°C
+                t2_hp = 20 #°C
+                q1_hp = 26700 #W
+                q2_hp = 72000 #W
+                cop1 = 1.73
+                cop2 = 3.6
+
+                result['hp_air'] = 'MT HeatPump (Dimplex LA 60TU)'
+
+
+            elif tmax_dhw > 65:
+                print('HT HeatPump')
+
+            plt.plot([t_min_env, 20],
+                     [q_max_distr, 0], 'b', [t2_hp, t1_hp], [q2_hp, q1_hp], 'r')
+            plt.text(t1_hp + 2, q1_hp, 'HeatPump')
+            plt.text(t_min_env + 2, q_max_distr, 'aggregated heat demand of district')
+            plt.xlabel('Outside Temp. / °C')
+            plt.ylabel('Thermal Power / kW')
+            plt.title('Bivalenzpunkt')
+            plt.show()
+
+            # Geradengleichung HeatPump bei minimaler Umgebungstemperatur
+            p_hp = (q2_hp - q1_hp) / (t2_hp - t1_hp) * (t_min_env - t1_hp) + q1_hp
+
+            # Geragengleichung Wärmebedarf
+            p_distr = q_max_distr
+
+            if 'boiler' in scenario['peak']:
+                result['boiler'] = p_distr - p_hp
+
+    if scenario['base'] == [] and scenario['peak'] == ['boiler']:
+        result['boiler'] = max(th_LDC)
+
+    # q_peak_max = []
+    # for device in scenario['peak']:
+    #     if device == 'boiler':
+    #         boiler_inst = max(th_LDC) - sum(q_base_max)
+    #         q_peak_max.append(boiler_inst)
+    #         # peak.append('Boiler: ' + str(round(boiler_inst/1000)) + 'kW (' + str(round(boiler_inst/max(th_curve)*100)) + '%)')
+    #         result['boiler'] = boiler_inst
+
+    return result
 
 
 
