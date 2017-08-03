313ce80ca7aa6dfc16b222775d712d6c7c39cfb8
Julius Unrath <julius.unrath@posteo.de>
2017-08-03 10:23:39 +0200

    every scenario gets "own" city object with device classes from pycity_calc implemented for chp, boiler, hp_aw choose_device function to select chp from dict (still todo: chp data in excel sheet)

--- pycity_calc/toolbox/dimensioning/dim_empirical_approach.py
+++ pycity_calc/toolbox/dimensioning/dim_empirical_approach.py
@@ -17,9 +17,22 @@ Passende Szenarien anhand der Randbedingungen auswählen und in qual_scenarios s
 
 import os
 import pickle
+import numpy as np
+from copy import deepcopy
+import xlrd
+
 import matplotlib.pyplot as plt
 import pycity_calc.cities.city as City
 from prettytable import PrettyTable
+
+import pycity.classes.supply.BES as BES
+import pycity.classes.supply.Boiler as Boiler
+import pycity.classes.supply.CHP as CHP
+import pycity.classes.supply.HeatPump as HP
+import pycity.classes.supply.ElectricalHeater as ElectricalHeater
+import pycity.classes.supply.Inverter as Inverter
+import pycity.classes.supply.PV as PV
+import pycity.classes.supply.ThermalEnergyStorage as ThermalEnergyStorage
 
 sc0 = {'type':['centralized','decentralized'],'base':['hp_geo'],'peak':['boiler']}
 sc1 = {'type':['centralized','decentralized'],'base':['hp_air'],'peak':['boiler']}
@@ -68,14 +81,10 @@ def run_approach(city):
         for scenario in all_scenarios:
             if 'centralized' in scenario['type']:
                 if approve_scenario(city, scenario, geothermal):
-                    solutions.append(dim_centralized(city,scenario))
+                    solutions.append(dim_centralized(deepcopy(city),scenario))
 
 
-        print('\n##### Centralized #####')
-        t = PrettyTable(['Scenario', 'devices'])
-        for idx, val in enumerate(solutions):
-            t.add_row(['Scenario ' + str(idx), val])
-        print(t)
+
 
     elif dhn_elig < 3:
         print('District Heating not eligible!')
@@ -139,6 +148,7 @@ def get_city_type(city):
             return 'big'
 
 
+# User input for more specifications
 def additional_information():
 
     print('Please clarify some facts about the district...')
@@ -276,6 +286,21 @@ def get_LDC(curve):
     return sorted(curve, reverse=True)
 
 
+def choose_device(dev_type, q_ideal):
+    # Source: BHKW-Kenndaten 2014, S.26
+    chp_list = {'vai1':[0.263, 0.658, 1000, 2500], 'vai2':[0.25, 0.667, 3000, 8000], 'vai3':[0.247,0.658,4700,12500],
+                'vie':[0.27, 0.671, 6000, 14900], 'rmb':[0.263, 0.657, 7200, 18000]} # [eta_el, eta_th, p_nom, q_nom]
+
+    if dev_type == 'chp':
+        specs = [0, 0, 0, 0]
+        for dev in chp_list.values():
+            eta_th = dev[1]
+            q_nom = dev[3]
+            if abs(q_nom*eta_th-q_ideal) < abs(specs[3]*eta_th-q_ideal):       # q_nom * eta?!
+                specs = dev[:]
+    return specs
+
+
 def dim_centralized(city, scenario):
     '''
     Set sizes of devices in scenario
@@ -283,19 +308,15 @@ def dim_centralized(city, scenario):
     :return: scenario with sizes of devices - in welcher form? city_object
     '''
 
+    [el_curve, th_curve] = city.get_power_curves(current_values=False)
+    th_LDC = get_LDC(th_curve)
+    eta_transmission = 0.9  # richtigen Faktor suchen
+
     building_age = get_building_age(city)
     if building_age == 'new':
         print('Check if requirements of EEWärmeG are fulfilled!')
 
-    result = {}
-
-    # get thermal and electrical demand curves (only available for sanitation)
-    [el_curve, th_curve] = city.get_power_curves(current_values=False)
-
-    # Gleichzeitigkeitsfaktor nicht benötigt, da konkrete Werte zur Dimensionierung benutzt werden
-
-    # Load duration curve (Jahresdauerlinie)
-    th_LDC = get_LDC(th_curve)
+    bes = BES.BES(city.environment)
 
     q_base_max = []
 
@@ -306,9 +327,13 @@ def dim_centralized(city, scenario):
             # BHKW immer mit Speicher
 
             # Dimensionierungsstrategie: 1.Anlage bei 5500h/a mit 1/2 mehr installierter Leistung als nötig für Speicher
-            chp_inst = 3/2*th_LDC[5500]
 
-            result['chp'] = chp_inst # in W
+
+            q_chp = 3/2*th_LDC[5500]/eta_transmission
+
+            [eta_el, eta_th, p_nom, q_nom] = choose_device('chp',q_chp)
+            chp = CHP.CHP(city.environment, p_nom, q_nom, eta_el+eta_th)
+            bes.addDevice(chp)
 
             # Dimensionierung nach Krimmling "Energieeffiziente Nahwärmesysteme", S.127 - Faustformel: BHKW-Leistung zwischen 10% und 20% der Maximallast
             # q_borders = [min(th_LDC, key=lambda x:abs(x-0.1*max(th_LDC))), min(th_LDC, key=lambda x:abs(x-0.2*max(th_LDC)))] #gibt die Leistungsgrenzen nach Faustformel aus
@@ -317,24 +342,82 @@ def dim_centralized(city, scenario):
             # print('Betriebszeiten zwischen ', t_borders)
 
             if 'boiler' in scenario['peak']:
-                boiler_inst = max(th_LDC) - chp_inst
-                result['boiler'] = boiler_inst
-
-
+                q_boiler = max(th_LDC) - q_nom
+                boiler = Boiler.Boiler(city.environment, q_boiler, 0.8)
+                bes.addDevice(boiler)
 
         elif device == 'solar': # Regeln für Dimensionierung einführen!
             pv_area = 0
             for building in city.node.values():
                 pv_area += building['entity'].roof_usabl_pv_area
-            result['solar'] = pv_area # in m2
+            print('Solarthermie auf ', pv_area, ' qm')
+            #result['solar'] = pv_area # in m2
 
         elif device == 'hp_geo':
-            result['hp_geo'] = 1 # dummy
+            print(' hp geothermie')
+            # result['hp_geo'] = 1 # dummy
 
 
 
         elif device == 'hp_air':
 
+            # erster Teil aus py_city -> examples übernommen (Daten für Dimplex_LA60TU angepasst)
+
+            #  Heatpump data path
+            this_path = os.path.dirname(os.path.abspath(__file__))
+            hp_data_path = os.path.join(this_path, 'input', 'heat_pumps.xlsx')
+            heatpumpData = xlrd.open_workbook(hp_data_path)
+            dimplex_LA60TU = heatpumpData.sheet_by_name("Dimplex_LA60TU")
+
+            # Size of the worksheet
+            number_rows = dimplex_LA60TU._dimnrows
+            number_columns = dimplex_LA60TU._dimncols
+            # Flow, ambient and max. temperatures
+            tFlow = np.zeros(number_columns - 2)
+            tAmbient = np.zeros(int((number_rows - 7) / 2))
+            tMax = dimplex_LA60TU.cell_value(0, 1)
+
+            firstRowCOP = number_rows - len(tAmbient)
+
+            qNominal = np.empty((len(tAmbient), len(tFlow)))
+            cop = np.empty((len(tAmbient), len(tFlow)))
+
+            for i in range(number_columns - 2):
+                tFlow[i] = dimplex_LA60TU.cell_value(3, 2 + i)
+
+            for col in range(len(tFlow)):
+                for row in range(len(tAmbient)):
+                    qNominal[row, col] = dimplex_LA60TU.cell_value(int(4 + row),
+                                                                   int(2 + col))
+                    cop[row, col] = dimplex_LA60TU.cell_value(int(firstRowCOP + row),
+                                                              int(2 + col))
+
+            pNominal = qNominal / cop
+
+            # Create HP
+            lower_activation_limit = 0.5
+
+            hp_air = HP.Heatpump(city.environment, tAmbient, tFlow, qNominal, pNominal, cop,
+                                 tMax, lower_activation_limit)
+
+            bes.addDevice(hp_air)
+
+            # Details HeatPump
+            t1_hp = -15  # °C
+            t2_hp = 20  # °C
+            q1_hp = hp_air.heat[1][2]  # W
+            q2_hp = hp_air.heat[7][2]  # W
+
+            # Leistung von HP bei niedrigster Temperatur (Annahme Wärmebedarf dort am größten)
+            q_hp = (q2_hp - q1_hp) / (t2_hp - t1_hp) * (min(city.environment.weather.tAmbient) - t1_hp) + q1_hp
+
+            if 'boiler' in scenario['peak']:
+                q_boiler = max(city.get_aggr_space_h_power_curve(current_values=False)) - q_hp
+                boiler = Boiler.Boiler(city.environment, q_boiler, 0.8)
+                bes.addDevice(boiler)
+
+
+            '''
             # Vorlauftemperaturen für Heizkörper? Fußbodenheizung?
             # check if tFlow = 60°C.
             tmax_dhw = 0
@@ -367,14 +450,16 @@ def dim_centralized(city, scenario):
             elif tmax_dhw > 65:
                 print('HT HeatPump')
 
-            plt.plot([t_min_env, 20],
-                     [q_max_distr, 0], 'b', [t2_hp, t1_hp], [q2_hp, q1_hp], 'r')
-            plt.text(t1_hp + 2, q1_hp, 'HeatPump')
-            plt.text(t_min_env + 2, q_max_distr, 'aggregated heat demand of district')
-            plt.xlabel('Outside Temp. / °C')
-            plt.ylabel('Thermal Power / kW')
-            plt.title('Bivalenzpunkt')
-            plt.show()
+            # -------- Heatpump und Heat demand plotten ---------
+            # plt.plot([t_min_env, 20],
+            #          [q_max_distr, 0], 'b', [t2_hp, t1_hp], [q2_hp, q1_hp], 'r')
+            # plt.text(t1_hp + 2, q1_hp, 'HeatPump')
+            # plt.text(t_min_env + 2, q_max_distr, 'aggregated heat demand of district')
+            # plt.xlabel('Outside Temp. / °C')
+            # plt.ylabel('Thermal Power / kW')
+            # plt.title('Bivalenzpunkt')
+            # plt.show()
+            # ----------------------------------------------------
 
             # Geradengleichung HeatPump bei minimaler Umgebungstemperatur
             p_hp = (q2_hp - q1_hp) / (t2_hp - t1_hp) * (t_min_env - t1_hp) + q1_hp
@@ -387,16 +472,15 @@ def dim_centralized(city, scenario):
 
     if scenario['base'] == [] and scenario['peak'] == ['boiler']:
         result['boiler'] = max(th_LDC)
+    '''
 
-    # q_peak_max = []
-    # for device in scenario['peak']:
-    #     if device == 'boiler':
-    #         boiler_inst = max(th_LDC) - sum(q_base_max)
-    #         q_peak_max.append(boiler_inst)
-    #         # peak.append('Boiler: ' + str(round(boiler_inst/1000)) + 'kW (' + str(round(boiler_inst/max(th_curve)*100)) + '%)')
-    #         result['boiler'] = boiler_inst
 
-    return result
+    # Im ersten Gebäude wird das BES installiert (-> besser in kürzester Distanz aller Gebäude)
+    assert not city.node[city.nodelist_building[0]]['entity'].hasBes, ('Building 0 has already BES. Mistakes may occur!')
+    city.node[city.nodelist_building[0]]['entity'].addEntity(bes)
+
+
+    return city
 
 
 
