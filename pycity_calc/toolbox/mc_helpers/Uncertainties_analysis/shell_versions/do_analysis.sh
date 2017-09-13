3e5fd738cebe2ffdf50d383c6175985f3861d058
laura.esling <laura.esling@eonerc.rwth-aachen.de>
2017-09-13 14:19:37 +0200

    delete not necessary files

--- pycity_calc/toolbox/mc_helpers/Uncertainties_analysis/shell_versions/do_analysis.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-python Uncertainties_script_Sc1.py
-
-python Uncertainties_script_Sc8.py
-python Uncertainties_script_bis.py
-python Uncertainties_script_bis1.py
-python Uncertainties_script_bis2.py
-
