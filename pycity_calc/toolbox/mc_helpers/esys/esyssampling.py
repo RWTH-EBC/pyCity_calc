46a4e316c5b05eaeb879e53694df944f15f54086
Jan Schiefelbein <jschiefelbein@eonerc.rwth-aachen.de>
2017-10-24 20:02:56 +0200

    Add further esys sampling #249

--- pycity_calc/toolbox/mc_helpers/esys/esyssampling.py
+++ pycity_calc/toolbox/mc_helpers/esys/esyssampling.py
@@ -138,6 +138,129 @@ def sample_boi_eff(nb_samples, mean=0.92, std=0.01):
     return array_boi_eff
 
 
+def sample_chp_omega(nb_samples, mean=0.9, std=0.02):
+    """
+    Returns samples for chp overall efficiency
+
+    Parameters
+    ----------
+    nb_samples : int
+        Number of samples
+    mean : float, optional
+        Mean value of efficiency
+        (default: 0.9)
+    std : float, optional
+        Standard deviation of efficiency
+        (default: 0.02)
+
+    Returns
+    -------
+    array_chp_eff : np.array (of floats)
+        Numpy array holding samples of boiler efficiencies
+    """
+
+    assert nb_samples > 0
+    assert mean > 0
+    assert std >= 0
+
+    array_chp_eff = np.random.normal(loc=mean, scale=std,
+                                     size=nb_samples)
+
+    for i in range(len(array_chp_eff)):
+        assert array_chp_eff[i] <= 1
+
+    return array_chp_eff
+
+
+def sample_quality_grade_hp_bw(nb_samples, minv=0.45, maxv=0.55):
+    """
+    Returns samples for brine/water (or water/water) heat pump quality
+    grades.
+
+    Parameters
+    ----------
+    nb_samples : int
+        Number of samples
+    minv : float
+        Minimum value (default: 0.45)
+    maxv : float
+        Maximum value (default: 0.55)
+
+    Returns
+    -------
+    array_hp_bw_qual : np.array (of float)
+        Numpy array with brine/water heat pump quality grade samples
+    """
+
+    assert nb_samples > 0
+    assert minv >= 0
+    assert maxv >= 0
+
+    array_hp_bw_qual = \
+        np.random.uniform(low=minv, high=maxv, size=nb_samples)
+
+    return array_hp_bw_qual
+
+
+def sample_quality_grade_hp_bw(nb_samples, minv=0.45, maxv=0.55):
+    """
+    Returns samples for brine/water (or water/water) heat pump quality
+    grades.
+
+    Parameters
+    ----------
+    nb_samples : int
+        Number of samples
+    minv : float
+        Minimum value (default: 0.45)
+    maxv : float
+        Maximum value (default: 0.55)
+
+    Returns
+    -------
+    array_hp_bw_qual : np.array (of float)
+        Numpy array with brine/water heat pump quality grade samples
+    """
+
+    assert nb_samples > 0
+    assert minv >= 0
+    assert maxv >= 0
+
+    array_hp_bw_qual = \
+        np.random.uniform(low=minv, high=maxv, size=nb_samples)
+
+    return array_hp_bw_qual
+
+
+def sample_quality_grade_hp_aw(nb_samples, minv=0.32, maxv=0.4):
+    """
+    Returns samples for air/water heat pump quality grades
+
+    Parameters
+    ----------
+    nb_samples : int
+        Number of samples
+    minv : float
+        Minimum value (default: 0.32)
+    maxv : float
+        Maximum value (default: 0.4)
+
+    Returns
+    -------
+    array_hp_aw_qual : np.array (of float)
+        Numpy array with air/water heat pump quality grade samples
+    """
+
+    assert nb_samples > 0
+    assert minv >= 0
+    assert maxv >= 0
+
+    array_hp_aw_qual = \
+        np.random.uniform(low=minv, high=maxv, size=nb_samples)
+
+    return array_hp_aw_qual
+
+
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
