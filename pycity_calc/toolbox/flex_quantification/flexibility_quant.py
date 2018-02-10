b66d92f1b8d8ed199d42cbf036065b23872ac898
Jan Schiefelbein <jschiefelbein@eonerc.rwth-aachen.de>
2018-02-10 12:48:39 +0100

    Add t_delayed calculation #383

--- pycity_calc/toolbox/flex_quantification/flexibility_quant.py
+++ pycity_calc/toolbox/flex_quantification/flexibility_quant.py
@@ -123,6 +123,87 @@ def calc_t_forced_build(building, id=None):
     return array_t_forced
 
 
+def calc_t_delayed_build(building, id=None):
+    """
+    Calculate t delayed array for building
+
+    Parameters
+    ----------
+    building : object
+        Building object of pyCity_calc
+    id : int, optional
+        Building id (default: None)
+
+    Returns
+    -------
+    array_t_delayed : np.array
+        Array holding t delayed for each timestep.
+        t_delayed is given in seconds
+    """
+
+    #  Check if building has energy system
+    #  ###########################################################
+    if building.hasBes is False:
+        msg = 'Building ' + str(id) + ' has no building energy system! ' \
+                                      'Thus, cannot calculate t_forece array.'
+        raise AssertionError(msg)
+
+    timestep = building.environment.timer.timeDiscretization
+
+    #  Create initial array
+    array_t_delayed = np.zeros(int(365 * 24 * 3600 / timestep))
+
+    #  ###########################################################
+    if building.bes.hasTes is False:
+        msg = 'Building ' + str(id) + ' has no thermal storage. ' \
+                                      'Thus, therm. flexibility is zero.'
+        warnings.warn(msg)
+        #  Flexibility is zero, return array with zeros
+        return array_t_delayed
+
+    #  Extract thermal power curve of building
+    sh_power = building.get_space_heating_power_curve()
+    dhw_power = building.get_dhw_power_curve()
+    th_power = sh_power + dhw_power
+
+    #  Loop over each timestep
+    #  ###########################################################
+    for i in range(len(array_t_delayed)):
+        #  Copy storage and set initial / current temperature to t_max (full)
+        tes_copy = copy.deepcopy(building.bes.tes)
+        tes_copy.t_current = tes_copy.tMax
+        tes_copy.tInit = tes_copy.tMax
+
+        #  Initial timestep
+        for t in range(len(array_t_delayed) - i):
+
+            #  Current thermal power demand
+            th_pow_cur = th_power[t + i]
+
+            #  if calc_storage_q_in_max > q_ehg_nom
+            if tes_copy.calc_storage_q_out_max() > th_pow_cur:
+                t_prior = tes_copy.t_current
+
+                #  perform calc_storage_temp_for_next_timestep
+                tes_copy.calc_storage_temp_for_next_timestep(q_in=0,
+                                                             q_out=th_pow_cur,
+                                                             t_prior=t_prior,
+                                                             save_res=True,
+                                                             time_index=t)
+
+            else:
+                #  Reduce by one increment of t, if t > 0
+                if t > 0:
+                    t -= 1
+                #  End seconds for loop
+                break
+
+        #  Save to array_t_forced
+        array_t_delayed[i] = t * timestep  # in seconds
+
+    return array_t_delayed
+
+
 if __name__ == '__main__':
 
     import matplotlib.pyplot as plt
@@ -154,6 +235,8 @@ if __name__ == '__main__':
     #  Get list of buildings
     list_build_ids = city.get_list_build_entity_node_ids()
 
+    dict_array_t_forced = {}
+
     #  Loop over buildings
     for n in list_build_ids:
         #  Pointer to current building object
@@ -162,8 +245,30 @@ if __name__ == '__main__':
         #  Calculate t_force array
         array_t_forced = calc_t_forced_build(building=curr_build, id=n)
 
-        plt.plot(array_t_forced/3600)
-        plt.xlabel('Time in hours')
-        plt.ylabel('T_forced in hours')
-        plt.show()
-        plt.close()
+        dict_array_t_forced[n] = array_t_forced
+
+        if n == 1001 or n == 1004:
+            plt.plot(array_t_forced / 3600)
+            plt.xlabel('Time in hours')
+            plt.ylabel('T_forced in hours')
+            plt.show()
+            plt.close()
+
+    dict_array_t_delayed = {}
+
+    #  Loop over buildings
+    for n in list_build_ids:
+        #  Pointer to current building object
+        curr_build = city.nodes[n]['entity']
+
+        #  Calculate t_force array
+        array_t_delayed = calc_t_delayed_build(building=curr_build, id=n)
+
+        dict_array_t_delayed[n] = array_t_delayed
+
+        if n == 1001 or n == 1004:
+            plt.plot(array_t_delayed / 3600)
+            plt.xlabel('Time in hours')
+            plt.ylabel('T_delayed in hours')
+            plt.show()
+            plt.close()
