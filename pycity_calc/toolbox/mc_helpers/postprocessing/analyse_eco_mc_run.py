d8ce8d13156a55b6a3fa637b80b543694d4c006e
Jan Schiefelbein <jschiefelbein@eonerc.rwth-aachen.de>
2018-06-08 09:43:49 +0200

    Correct min/max usage for risk friendly parameters in analyse_eco_mc_run.py #467

--- pycity_calc/toolbox/mc_helpers/postprocessing/analyse_eco_mc_run.py
+++ pycity_calc/toolbox/mc_helpers/postprocessing/analyse_eco_mc_run.py
@@ -1065,10 +1065,10 @@ class EcoMCRunAnalyze(object):
             obj = 'min'
         elif type == 'dimless_an':
             array_in = self._array_dimless_cost
-            obj = 'max'
+            obj = 'min'
         elif type == 'dimless_co2':
             array_in = self._array_dimless_co2
-            obj = 'max'
+            obj = 'min'
 
         risk_friendly_factor = self. \
             calc_risk_friendly_factor(array_in=array_in,
